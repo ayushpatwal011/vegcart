1134d0e95febd6927a345c9f4d247f20c4de581a
ayush patwal <137012714+ayushpatwal011@users.noreply.github.com>
2025-05-29 16:28:57 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <div className="hidden sm:flex items-center gap-8 text-md">
         <NavLink to="/">Home</NavLink>
-        <NavLink to="/product">All Product</NavLink>
+        <NavLink to="/products">All Product</NavLink>
         <NavLink to="/">Contact</NavLink>
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
